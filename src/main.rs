fa21dd9247c3f97b680ae86a876442a0878938e4
Wackyator <pratapsinghtarun9@gmail.com>
2021-02-27 20:53:13 +0530

    aaa

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use url::Url;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
 }
 
 struct Client {
@@ -51,6 +52,33 @@ impl Client {
     }
 }
 
+struct Tunnel<T> { //replace generic with concrete type for websocket
+    ws_uri: String,
+    http_uri: String,
+    client: Option<Client>,
+    ws_client: Option<T>,
+}
+
+impl Tunnel {
+    fn new(ws_uri: String, http_uri: String) -> Self {
+        Self {
+            ws_uri,
+            http_uri,
+            client: None,
+            ws_client: None,
+        }
+    }
+}
+
+impl Tunnel {
+    async fn open_tunnel(&mut self) {
+        self.client = Client::new(self.http_uri, token);
+        loop {
+            todo!();
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 struct Message {
     id: String,
@@ -61,7 +89,28 @@ struct Message {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-ServerInfo {
+struct ServerInfo {
     host: String,
     token: String,
+}
+
+#[derive(Debug)]
+struct ResponseObject {
+    request_id: String,
+    token: String,
+    status: u16,
+    header: HashMap<String, String>,
+    body: HashMap<String, String>,
+}
+
+impl ResponseObject {
+    fn new(request_id: String, token: String, status: u16, header: HashMap<String, String>, body: HashMap<String, String>) -> Self {
+        Self {
+            request_id,
+            token,
+            status,
+            header,
+            body,
+        }
+    }
 }
