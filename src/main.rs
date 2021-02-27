c6a537f769654208482ba7bc800e18300008b3d8
Wackyator <pratapsinghtarun9@gmail.com>
2021-02-27 20:37:33 +0530

    add Client struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+extern crate serde;
+extern crate websocket;
+extern crate openssl;
+extern crate url;
+
+use serde::{Serialize, Deserialize};
+use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+}
+
+struct Client {
+    base_uri: String,
+    token: String,
+    http_client: reqwest::Client,
+}
+
+impl Client {
+    fn new(base_uri: String, token: String) -> Self {
+        Self {
+            base_uri,
+            token,
+            http_client: reqwest::Client::new(),
+        }
+    }
+}
+
+impl Client {
+    async fn process<T>(&self, msg: Message, websocket: T) { //change frm generic to concrete type later
+        let url = Url::parse(&self.base_uri).join(&msg.url);
+
+        let resp: self.http_client.request(&msg.method, url)
+            .headers(&msg.header)
+            .json(&msg.body)
+            .send()
+            .await
+            .json::<HashMap<String, String>>()
+            .await;
+
+        if resp.status() < 400 {
+            let resp = ResponseObject::new(msg.id, self.token, resp.status(), resp.headers().into(), resp);
+            //send the response to server with the socket stream
+        }
+        
+        todo!();
+
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Message {
+    id: String,
+    method: String,
+    url: String,
+    header: Option<HashMap<String, String>>,
+    body: Option<HashMap<String, String>>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+ServerInfo {
+    host: String,
+    token: String,
 }
